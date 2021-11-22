646e9714e3d5798e09dea5ae2469b2f6ff48cdf7
Thomas Wormald <thomas.wormald@viagogo.com>
2021-11-22 21:34:32 +0000

    Add grpc service, add mudblazor and fluxor

--- src/GrpcBrowser/Program.cs
+++ src/GrpcBrowser/Program.cs
@@ -1,13 +1,15 @@
-using GrpcBrowser.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Fluxor;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddServerSideBlazor(); 
+builder.Services.AddMudServices();
+builder.Services.AddFluxor(options => options.ScanAssemblies(typeof(Program).Assembly).UseReduxDevTools());
 
 var app = builder.Build();
 
