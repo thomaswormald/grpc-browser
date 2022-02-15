50ae8c38422e878f0c8ef896e7b184bc8a701e54
Thomas Wormald <thomas.wormald@viagogo.com>
2022-02-15 22:47:59 +0000

    WIP Fx Rate sample

---
+++ src/Samples/FxService/Api/Fx/FxService.cs
@@ -0,0 +1,84 @@
+﻿using ProtoBuf.Grpc;
+using ProtoBuf.Grpc.Configuration;
+using System.Reactive;
+using System.Reactive.Linq;
+
+[Service]
+public interface IFxApi
+{
+    [Operation]
+    Task<FxOrderResult> PlaceFxOrder(FxOrder request, CallContext context = default);
+
+    [Operation]
+    IAsyncEnumerable<FxRateUpdate> StreamFxRates(IAsyncEnumerable<FxRateRequest> request, CallContext context = default);
+
+    [Operation]
+    Task<SetFxRateResult> SetFxRates(IAsyncEnumerable<SetFxRateRequest> requestStream, CallContext context = default);
+}
+
+public class FxApi : IFxApi
+{
+    private readonly IFxRepository _fxRepository;
+    private readonly IAccountRepository _accountRepository;
+
+    public FxApi(IFxRepository fxRepository, IAccountRepository accountRepository)
+    {
+        _fxRepository = fxRepository;
+        _accountRepository = accountRepository;
+    }
+
+    public async Task<FxOrderResult> PlaceFxOrder(FxOrder request, CallContext context = default)
+    {
+        // Good job I don't work for a bank, this is not safe at all. Bank repository needs to withdraw and deposit in one transaction
+
+        var fxRate = await _fxRepository.GetFxRate(request.FromCurrency, request.ToCurrency);
+        
+        if (fxRate is null)
+        {
+            return new FxOrderResult
+            {
+                Success = false,
+                ErrorMessage = $"Unable to find FX Rate from {request.FromCurrency} to {request.ToCurrency}"
+            };
+        }
+
+        var sufficientFunds = await _accountRepository.Withdraw(request.FromCurrency, request.AmountInFromCurrency);
+
+        if (!sufficientFunds)
+        {
+            return new FxOrderResult
+            {
+                Success = false,
+                ErrorMessage = $"Insufficient funds to withdraw {request.FromCurrency} {request.AmountInFromCurrency}"
+            };
+        }
+
+        var convertedAmount = fxRate.Rate * request.AmountInFromCurrency;
+
+        await _accountRepository.Deposit(request.ToCurrency, convertedAmount);
+
+        return new FxOrderResult
+        {
+            Success = true,
+            OrderId = Guid.NewGuid()
+        };
+    }
+
+    public async Task<SetFxRateResult> SetFxRates(IAsyncEnumerable<SetFxRateRequest> requestStream, CallContext context = default)
+    {
+        await foreach (var fxRate in requestStream)
+        {
+            await _fxRepository.SetFxRate(new FxRate(fxRate.FromCurrency, fxRate.ToCurrency, fxRate.ConversionRate));
+        }
+
+        return new SetFxRateResult
+        {
+            FxRates = await _fxRepository.GetAllFxRates()
+        };
+    }
+
+    public IAsyncEnumerable<FxRateUpdate> StreamFxRates(IAsyncEnumerable<FxRateRequest> request, CallContext context = default)
+    {
+        Observable.
+    }
+}
---
+++ src/Samples/FxService/Data/FxRepository.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Immutable;
+using System.Reactive.Subjects;
+
+public interface IFxRepository
+{
+    IObservable<FxRate> FxRateObservable { get; }
+    Task SetFxRate(FxRate rate);
+    Task<FxRate?> GetFxRate(string FromCurrency, string ToCurrency);
+    Task<ImmutableDictionary<(string, string), decimal>> GetAllFxRates();
+}
+
+public record FxRate(string FromCurrency, string ToCurrency, decimal Rate);
+
+public class FxRepository : IFxRepository
+{
+    private readonly Subject<FxRate> _fxRateSubject;
+    private ImmutableDictionary<(string, string), decimal> _rates;
+
+    public FxRepository()
+    {
+        _rates = ImmutableDictionary<(string, string), decimal>.Empty;
+        _fxRateSubject = new Subject<FxRate>();
+
+        _fxRateSubject.Subscribe(update =>
+        {
+            _rates = _rates.SetItem((update.FromCurrency.ToUpper(), update.ToCurrency.ToUpper()), update.Rate);
+        });
+    }
+
+    public IObservable<FxRate> FxRateObservable => _fxRateSubject;
+
+    public async Task SetFxRate(FxRate rate)
+    {
+        _fxRateSubject.OnNext(rate);
+    }
+
+    public Task<FxRate?> GetFxRate(string FromCurrency, string ToCurrency)
+    {
+        return Task.FromResult(
+            _rates.TryGetValue((FromCurrency.ToUpper(), ToCurrency.ToUpper()), out var result)
+            ? new FxRate(FromCurrency, ToCurrency, result)
+            : null);
+    }
+
+    public Task<ImmutableDictionary<(string, string), decimal>> GetAllFxRates()
+    {
+        return Task.FromResult(_rates);
+    }
+}
